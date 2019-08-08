f93e81018c22871e046998a5a0fb1987e5cd2fc1
Kian Van Riel <kianvanriel107@gmail.com>
2019-08-08 14:45:28 +0200

    test

--- includes/nav.php
+++ includes/nav.php
@@ -10,10 +10,6 @@
 if (isUserLoggedIn()) {
 ?>
   <nav>
-    <ul>
-      <li class="nav-item"><a href="dashboard" <?php if('dashboard'===$config['serverinfo']['currentpagelocation']){echo "class=\"active\"";} ?>>dashboard</a></li>
-      <li class="nav-item"><a href="login?logout" <?php if('login'===$config['serverinfo']['currentpagelocation']){echo "class=\"active\"";} ?>>uitlogen</a></li>
-    </ul>
   </nav>
 <?php } ?>
 <main>
