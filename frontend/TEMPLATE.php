5f3d352200c500f39fb5d79e1a1852e39d57aa8c
onno204 <onno204@onno204.nl>
2019-08-08 04:28:17 +0200

    added template

---
+++ frontend/TEMPLATE.php
@@ -0,0 +1,13 @@
+<?php
+require_once "../config.php";
+doesUserHavePermission("dashboard", true, true);
+
+
+require_once "../includes/header.php";
+require_once "../includes/nav.php";
+?>
+
+<h1>CONTENT</h1>
+
+<?php
+require_once "../includes/footer.php";
