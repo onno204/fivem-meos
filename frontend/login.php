d447315ceab2ba3fd6e4e4ff0b1a9aa09d4ceaa6
Kian Van Riel <kianvanriel107@gmail.com>
2019-08-29 01:07:09 +0200

    Update login.php

--- frontend/login.php
+++ frontend/login.php
@@ -22,6 +22,7 @@ redirectToPage("dashboard");
     <img src="assets/img/achtergrond.png" alt="" />
 </div>
     <div class="login-status" id="loginFormResponseMessage"></div>
+    <h1 style="font-family: Bahnschrift; color: #FFF;">Inloggen</h1>
     <input class="inputfield" type="text" name="username" placeholder="Gebruikersnaam">
     <p></p>
     <input class="inputfield" type="password" name="password" placeholder="Wachtwoord">
