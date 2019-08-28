c5c41e8dfaab004668ed6b940a0d8ff9692e7c4b
Kian Van Riel <kianvanriel107@gmail.com>
2019-08-29 00:41:32 +0200

    Test2
    Dit is even een testje

--- frontend/login.php
+++ frontend/login.php
@@ -17,6 +17,7 @@ redirectToPage("dashboard");
 }
 ?>
 <form class="box" id="loginForm" attr-action="login" attr-type="login">
+    <img class="focus-logo" src="focus.png" width="430" height="117">    
     <div class="login-status" id="loginFormResponseMessage"></div>
     <input class="inputfield" type="text" name="username" placeholder="Gebruikersnaam">
     <p></p>
