a88a1a4d022f00e9789e1a1d079bd235de69af89
Kian Van Riel <kianvanriel107@gmail.com>
2019-09-18 11:20:05 +0200

    Test

--- includes/nav.php
+++ includes/nav.php
@@ -2,21 +2,10 @@
 
 <div class="kop-achtergrond">
     <img class= "politielogo1" src="assets/img/politielogo1.png" width="121" height="31">
-    <a href="dashboard" <?php if('dashboard'===$config['serverinfo']['currentpage']){echo "class=\"active\"";} ?> >Dashboard</a>
+    <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+    </ul>
 </div>
-
-<?php
-// Only show nav if user is loggedin
-if (isUserLoggedIn()) {
-?>
-    <nav class="nav-collaps">
-        <a href="dashboard" <?php if('dashboard'===$config['serverinfo']['currentpage']){echo "class=\"active\"";} ?> ><label class="fas fa-columns"></label><span>Dashboard</span></a>
-        <a href="persoon" <?php if('persoon'===$config['serverinfo']['currentpage']){echo "class=\"active\"";} ?> ><label class="fas fa-user"></label><span>Persoon</span></a>
-        <a href="voertuig" <?php if('voertuig'===$config['serverinfo']['currentpage']){echo "class=\"active\"";} ?> ><label class="fas fa-car"></label><span>Voertuig</span></a>
-        <a href="meldingen" <?php if('meldingen'===$config['serverinfo']['currentpage']){echo "class=\"active\"";} ?> ><label class="fas fa-book"></label><span>Meldingen</span></a>
-        <a href="instellingen" <?php if('instellingen'===$config['serverinfo']['currentpage']){echo "class=\"active\"";} ?> ><label class="fas fa-cogs"></label><span>Instellingen</span></a>
-        <a href="login?logout" <?php if('login'===$config['serverinfo']['currentpage']){echo "class=\"active\"";} ?> ><label class="fas fa-sign-out-alt"></label><span>Uitloggen</span></a>
-        <a class="back" onclick="toggleNavBar()"><label class="fa fa-chevron-left"></label></a>
-    </nav>
-<?php } ?>
-<main>
