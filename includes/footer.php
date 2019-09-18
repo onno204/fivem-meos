70904ec0595f9164b0fd771fe3e843ff4e235b24
Kian Van Riel <kianvanriel107@gmail.com>
2019-09-18 11:40:00 +0200

    Copyright Text

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer>
-    <?php echo date("Y"); ?> &copy; Copyright By Kian & Onno
+    <?php echo date("Y"); ?> &copy; Copyright Brussel Roleplay | Alle Rechten Voorbehouden.
 </footer>
 </body>
 </html>
