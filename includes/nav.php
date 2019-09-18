66c7acbb32d0ed7d898a09f3883abadd31312e49
Kian Van Riel <kianvanriel107@gmail.com>
2019-09-18 11:16:18 +0200

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -2,12 +2,6 @@
 
 <div class="kop-achtergrond">
     <img class= "politielogo1" src="assets/img/politielogo1.png" width="121" height="31">
-    <ul>
-         <li><a class="active" href="#home">Home</a></li>
-         <li><a href="#news">News</a></li>
-         <li><a href="#contact">Contact</a></li>
-         <li><a href="#about">About</a></li>
-    </ul>
 </div>
 
 <?php
