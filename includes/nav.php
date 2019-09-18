239a1e44bd3c371f42e9bea234c4ddfe6eaf7699
Kian Van Riel <kianvanriel107@gmail.com>
2019-09-18 11:26:33 +0200

    TEst

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 
 
 <div class="kop-achtergrond">
-    <img class= "politielogo1" src="assets/img/politielogo1.png" width="121" height="31">
+    <img class= "politielogo1" src="assets/img/politielogo1.png" width="80" height="71">
 </div>
 
 <?php
